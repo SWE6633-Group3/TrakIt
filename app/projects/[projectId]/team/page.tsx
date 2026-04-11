809dc6322d1aea81ba18fb8ca1d4580b405d6851
JoePent <joepent18@gmail.com>
2026-04-11 19:25:31 -0400

    first commit

--- app/projects/[projectId]/team/page.tsx
+++ app/projects/[projectId]/team/page.tsx
@@ -98,6 +98,7 @@ export default function ProjectTeamPage() {
     loadProjectName();
     loadTeam();
   }, [loadProjectName, loadTeam]);
+  
 
   const handleCreate = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
